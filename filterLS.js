605344149f92b16b8bfbee4fce5be055bba35266
mpmckenna8 <mpmckenna8@me.com>
2014-01-08 11:18:09 -0800

    learningmenode

--- filterLS.js
+++ filterLS.js
@@ -4,7 +4,10 @@ var path = require('path');
 
 fs.readdir(process.argv[2], function(err, files ){
 	for(i=0; i < files.length; i++){
-	console.log(files[i])
+	if(path.extname(files[i])===("."+ process.argv[3]) ){
+		console.log(files[i]);
 }
 
+}
+//console.log(process.argv[3])
 })
