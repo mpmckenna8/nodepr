5520e012d3039a15c7ff795d74f62f8d580d1572
Matthew McKenna <mpmckenna8@me.com>
2014-02-26 15:02:03 -0800

    Starting stream-adventure
    Getting started learning me about node streams interactively.

---
+++ streamsH/readstream.js
@@ -0,0 +1,16 @@
+var Stream = require('stream');
+var stream = new Stream;
+stream.readable = true;
+
+var c = 64;
+var iv = setInterval(function () {
+                     if (++c >= 100) {
+                     clearInterval(iv);
+                     stream.emit('end');
+                     }
+                     else stream.emit('data', String.fromCharCode(c));
+                     }, 100);
+
+stream.pipe(process.stdout);
+
+//Not terribly exciting example of getting data charcode in process and printing it to the console w/ little delays between each character. https://github.com/substack/stream-handbook
