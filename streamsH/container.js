a2cdfe1c51b307b4d481952e590b3b0fc641bb22
Matthew McKenna <mpmckenna8@me.com>
2014-03-29 13:38:31 -0700

    cheated to complete the container.js challenge stream-adventure
    I did learn about how to use node core to zip jam up though!

--- streamsH/container.js
+++ streamsH/container.js
@@ -1,14 +1,39 @@
 var through = require('through');
 var split = require('split');
-
+var zlib = require('zlib');
 var combine = require('stream-combiner')
 
     module.exports = function () {
       var grouper = through(write,end);
+      var current;
+
+      function write (line){
+        if (line.length === 0) return;
+        var row = JSON.parse(line);
+
+        if (row.type === 'genre'){
+          if(current){
+            this.queue(JSON.stringify(current) + '\n');
+          }
+          current = {name:row.name,books:[]};
+        }
+        else if(row.type ==='book'){
+          current.books.push(row.name);
+        }
+      }
+
+      function end(){
+        if(current){this.queue(JSON.stringify(current)+ '\n');
+      }
+      this.queue(null);
+      }
 
         return combine(
             // read newline-separated json,
             // group books into genres,
             // then gzip the output
+            split(), grouper, zlib.createGzip()
         );
     }
+
+// I was a cheater cheater pumpkin eater for this one!
